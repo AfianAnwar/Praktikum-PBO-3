cdfb25efd8421b81fe04f412439feceb096c63ca
AfianAnwar <afiananwar2@gmail.com>
2022-04-07 09:29:50 +0700

    first commit

---
+++ src/main/java/id/ac/uin/student/StudentApplication.java
@@ -0,0 +1,12 @@
+package id.ac.uin.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(id.ac.uin.student.StudentApplication.class, args);
+    }
+}
